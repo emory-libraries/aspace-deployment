6ae6b4873afe436013ae0b99debada9f14b71e7b
bwatson78 <brad.watson.orlando@gmail.com>
2023-02-13 09:04:09 -0500

    Adds in the container_management_labels fork.

--- development/config.rb
+++ development/config.rb
@@ -1,2 +1,33 @@
-AppConfig[:plugins] = ['aspace_static_plugins']
+AppConfig[:plugins] = ['aspace_static_plugins', 'container_management_labels']
 AppConfig[:emory_ga_tracking_public] = ''
+AppConfig[:container_management_labels_pagesize] = {
+    "avery-5163" => {
+        "size" => "letter",
+        "margin" => "0.5in 0.125in"},
+    "avery-22513" => {
+        "size" => "letter",
+        "margin" => "0.5in 0.125in"}
+}
+AppConfig[:container_management_labels] = [
+    {"institution_name" => {
+        "checked" => true,
+        "disabled" => false}},
+    {"repository_name" => {
+        "checked" => true,
+        "disabled" => false}},
+    {"resource_id" => {
+        "checked" => true,
+        "disabled" => false}},
+    {"agent_name" => {
+        "checked" => true,
+        "disabled" => false}},
+    {"resource_title" => {
+        "checked" => true,
+        "disabled" => false}},
+    {"type" => {
+        "checked" => true,
+        "disabled" => false}},
+    {"indicator" => {
+        "checked" => true,
+        "disabled" => true}}
+]
