92a1e125cdb35734ac734d901e7b545d3082801e
bwatson78 <brad.watson.orlando@gmail.com>
2025-06-24 12:23:40 -0500

    Preparing for ASpace v4.1.0 deployment.

--- development/config.rb
+++ development/config.rb
@@ -1,10 +1,10 @@
 AppConfig[:plugins] = ['aspace_static_plugins', 'container_management_labels', 'lcnaf', 'aeon-fulfillment-plugin', 'uga-archivesspace-reports']
 AppConfig[:public_url] = "http://localhost:3001"
-# AppConfig[:archivesspace_version] = "3.5.0"
+# AppConfig[:archivesspace_version] = "4.1.0"
 AppConfig[:emory_ga_tracking_public] = ''
 AppConfig[:pui_branding_img] = '/assets/images/shield.svg'
 AppConfig[:pui_branding_img_alt_text] = 'Emory University Libraries Logo'
-AppConfig[:emory_plugins_version] = 'v2024-07-05'
+AppConfig[:emory_plugins_version] = 'v2025-06-24'
 AppConfig[:container_management_labels_pagesize] = {
     "avery-5163" => {
         "size" => "letter",
